3df2c3465a978f37526113640ae4f76b580e26f4
Thinh Nguyen <hoangthinh.vn44@gmail.com>
2021-11-18 10:10:01 +0700

    first commit

---
+++ src/features/Weather/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+WeatherFeature.propTypes = {};
+
+function WeatherFeature(props) {
+	return <div>weather</div>;
+}
+
+export default WeatherFeature;
