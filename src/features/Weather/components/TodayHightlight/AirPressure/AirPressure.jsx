24deae81aa541ff715eb1c448769af7ba9f1e103
Thinh Nguyen <hoangthinh.vn44@gmail.com>
2021-11-19 15:01:34 +0700

    done desktop

--- src/features/Weather/components/TodayHightlight/AirPressure/AirPressure.jsx
+++ src/features/Weather/components/TodayHightlight/AirPressure/AirPressure.jsx
@@ -6,7 +6,7 @@ function AirPressure({ airpressure }) {
 		<div className='airpressure-control d-flex background-darkslategray'>
 			<h2 className='today-hightlight-title'>Air Pressure</h2>
 			<h2 className='d-flex airpressure-title'>
-				{airpressure} <p className='airpressure-unit'>mb</p>
+				{Math.trunc(airpressure)} <p className='airpressure-unit'>mb</p>
 			</h2>
 		</div>
 	);
